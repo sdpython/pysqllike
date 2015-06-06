2c7f58f0313d56091b4aac29ff6a3b2ac54b38a9
dupre <xavier.dupre@ensae.fr>
2015-06-06 11:43:53 +0200

    update license variable

--- src/pysqllike/__init__.py
+++ src/pysqllike/__init__.py
@@ -9,7 +9,7 @@ __author__ = "Xavier Dupré"
 __github__ = "https://github.com/sdpython/pysqllike"
 __url__ = "http://www.xavierdupre.fr/app/pysqllike/helpsphinx/index.html"
 __downloadUrl__ = "http://www.xavierdupre.fr/site2013/index_code.html#pysqllike"
-__license__ = "BSD License"
+__license__ = "MIT License"
 
 
 def _setup_hook():
