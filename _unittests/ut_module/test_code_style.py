71980ba02382b33142de0ea554a7cd2107e33e7a
xavier dupré <xavier.dupre@gmail.com>
2021-10-13 00:01:59 +0200

    Update test_code_style.py

--- _unittests/ut_module/test_code_style.py
+++ _unittests/ut_module/test_code_style.py
@@ -2,10 +2,8 @@
 @brief      test log(time=0s)
 """
 
-import sys
 import os
 import unittest
-import warnings
 from pyquickhelper.loghelper import fLOG
 from pyquickhelper.pycode import check_pep8
 from pyquickhelper.pycode.utils_tests_helper import _extended_refactoring
@@ -19,16 +17,12 @@ class TestCodeStyle(unittest.TestCase):
             self._testMethodName,
             OutputPrint=__name__ == "__main__")
 
-        if sys.version_info[0] == 2:
-            warnings.warn(
-                "skipping test_code_style because of Python 2 or " + sys.executable)
-            return
-
         thi = os.path.abspath(os.path.dirname(__file__))
         src_ = os.path.normpath(os.path.join(thi, "..", "..", "src"))
         check_pep8(src_, fLOG=fLOG, extended=[("fLOG", _extended_refactoring)],
                    pylint_ignore=('C0103', 'C1801', 'R0201', 'R1705', 'W0108', 'W0613',
-                                  'W0231', 'W0212', 'C0111', 'W0107', 'R1728'),
+                                  'W0231', 'W0212', 'C0111', 'W0107', 'R1728',
+                                  'C0209'),
                    skip=["Redefining built-in 'iter'",
                          "iter_rows.py:340",
                          "translation_class.py",
@@ -46,17 +40,12 @@ class TestCodeStyle(unittest.TestCase):
             self._testMethodName,
             OutputPrint=__name__ == "__main__")
 
-        if sys.version_info[0] == 2:
-            warnings.warn(
-                "skipping test_code_style because of Python 2 or " + sys.executable)
-            return
-
         thi = os.path.abspath(os.path.dirname(__file__))
         test = os.path.normpath(os.path.join(thi, "..", ))
         check_pep8(test, fLOG=fLOG, neg_pattern="temp_.*",
                    pylint_ignore=('C0111', 'C0103', 'W0622', 'C1801', 'C0412',
                                   'R0201', 'W0122', 'W0123', 'E1101', 'R1705',
-                                  'W0107', 'R1720'),
+                                  'W0107', 'R1720', 'C0209'),
                    skip=[],
                    extended=[("fLOG", _extended_refactoring)])
 
